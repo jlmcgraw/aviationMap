bec5f93701373428bb02ff23d7eb9f9bcb099cbe
Jesse McGraw <jlmcgraw@gmail.com>
2017-11-02 08:02:28 -0400

    Fixes for getting data

--- freshenLocalData.sh
+++ freshenLocalData.sh
@@ -9,17 +9,12 @@ IFS=$(printf '\n\t')   # IFS is newline or tab
 dataUrl="https://www.dropbox.com/s/bk6xvyht9q476k4/data.tar.xz?dl=0"
 dataFile="data.tar.xz"
 
-#Get all of the latest info
+# Get all of the latest info
 echo "Get the data archive"
-set +e
-wget --timestamping -erobots=off $dataUrl
-set -e
+wget --timestamping -erobots=off $dataUrl --output-document="$dataFile"
 
+# Extract the archive locally
 echo "Uncompress $dataFile"
-
-#Disable quit on error
-set +e
 tar --extract --verbose --keep-newer-files --file "$dataFile"
-#Restore quit on error
-set -e
+
 
